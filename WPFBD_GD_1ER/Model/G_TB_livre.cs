b0ce1f6d6bcd5934177a362b863810449fcdef88
Gaël DOCQUIER <Gael.DOCQUIER@student.hel.be>
2021-11-06 16:33:39 +0100

    version 5: amélioration encodage client + HTML stock + location + modification MDF (mail client)

--- WPFBD_GD_1ER/Model/G_TB_livre.cs
+++ WPFBD_GD_1ER/Model/G_TB_livre.cs
@@ -1,35 +1,42 @@
 #region Ressources extérieures
+
 using System;
 using System.Collections.Generic;
-using System.Text;
-using WPFBD_GD_1ER.Model;
 
-#endregion
+#endregion Ressources extérieures
 
 namespace WPFBD_GD_1ER.Model
 {
- /// <summary>
- /// Couche intermédiaire de gestion (Business Layer)
- /// </summary>
- public class G_TB_livre : G_Base
- {
-  #region Constructeurs
-  public G_TB_livre()
-   : base()
-  { }
-  public G_TB_livre(string sChaineConnexion)
-   : base(sChaineConnexion)
-  { }
-  #endregion
-  public int Ajouter(string titre, string auteur, int? ID_categorie, Int16 statut, int? Ann_pub, int ID_edition)
-  { return new A_TB_livre(ChaineConnexion).Ajouter(titre, auteur, ID_categorie, statut, Ann_pub, ID_edition); }
-  public int Modifier(int ID_livre, string titre, string auteur, int? ID_categorie, Int16 statut, int? Ann_pub, int ID_edition)
-  { return new A_TB_livre(ChaineConnexion).Modifier(ID_livre, titre, auteur, ID_categorie, statut, Ann_pub, ID_edition); }
-  public List<C_TB_livre> Lire(string Index)
-  { return new A_TB_livre(ChaineConnexion).Lire(Index); }
-  public C_TB_livre Lire_ID(int ID_livre)
-  { return new A_TB_livre(ChaineConnexion).Lire_ID(ID_livre); }
-  public int Supprimer(int ID_livre)
-  { return new A_TB_livre(ChaineConnexion).Supprimer(ID_livre); }
- }
+    /// <summary>
+    /// Couche intermédiaire de gestion (Business Layer)
+    /// </summary>
+    public class G_TB_livre : G_Base
+    {
+        #region Constructeurs
+
+        public G_TB_livre()
+         : base()
+        { }
+
+        public G_TB_livre(string sChaineConnexion)
+         : base(sChaineConnexion)
+        { }
+
+        #endregion Constructeurs
+
+        public int Ajouter(string titre, string auteur, int? ID_categorie, Int16 statut, int? Ann_pub, int ID_edition)
+        { return new A_TB_livre(ChaineConnexion).Ajouter(titre, auteur, ID_categorie, statut, Ann_pub, ID_edition); }
+
+        public int Modifier(int ID_livre, string titre, string auteur, int? ID_categorie, Int16 statut, int? Ann_pub, int ID_edition)
+        { return new A_TB_livre(ChaineConnexion).Modifier(ID_livre, titre, auteur, ID_categorie, statut, Ann_pub, ID_edition); }
+
+        public List<C_TB_livre> Lire(string Index)
+        { return new A_TB_livre(ChaineConnexion).Lire(Index); }
+
+        public C_TB_livre Lire_ID(int ID_livre)
+        { return new A_TB_livre(ChaineConnexion).Lire_ID(ID_livre); }
+
+        public int Supprimer(int ID_livre)
+        { return new A_TB_livre(ChaineConnexion).Supprimer(ID_livre); }
+    }
 }
--- WPFBD_GD_1ER/Model/G_TB_location.cs
+++ WPFBD_GD_1ER/Model/G_TB_location.cs
@@ -1,35 +1,42 @@
 #region Ressources extérieures
+
 using System;
 using System.Collections.Generic;
-using System.Text;
-using WPFBD_GD_1ER.Model;
 
-#endregion
+#endregion Ressources extérieures
 
 namespace WPFBD_GD_1ER.Model
 {
- /// <summary>
- /// Couche intermédiaire de gestion (Business Layer)
- /// </summary>
- public class G_TB_location : G_Base
- {
-  #region Constructeurs
-  public G_TB_location()
-   : base()
-  { }
-  public G_TB_location(string sChaineConnexion)
-   : base(sChaineConnexion)
-  { }
-  #endregion
-  public int Ajouter(int ID_client, DateTime? dat_location)
-  { return new A_TB_location(ChaineConnexion).Ajouter(ID_client, dat_location); }
-  public int Modifier(int ID_location, int ID_client, DateTime? dat_location)
-  { return new A_TB_location(ChaineConnexion).Modifier(ID_location, ID_client, dat_location); }
-  public List<C_TB_location> Lire(string Index)
-  { return new A_TB_location(ChaineConnexion).Lire(Index); }
-  public C_TB_location Lire_ID(int ID_location)
-  { return new A_TB_location(ChaineConnexion).Lire_ID(ID_location); }
-  public int Supprimer(int ID_location)
-  { return new A_TB_location(ChaineConnexion).Supprimer(ID_location); }
- }
+    /// <summary>
+    /// Couche intermédiaire de gestion (Business Layer)
+    /// </summary>
+    public class G_TB_location : G_Base
+    {
+        #region Constructeurs
+
+        public G_TB_location()
+         : base()
+        { }
+
+        public G_TB_location(string sChaineConnexion)
+         : base(sChaineConnexion)
+        { }
+
+        #endregion Constructeurs
+
+        public int Ajouter(int ID_client, DateTime? dat_location)
+        { return new A_TB_location(ChaineConnexion).Ajouter(ID_client, dat_location); }
+
+        public int Modifier(int ID_location, int ID_client, DateTime? dat_location)
+        { return new A_TB_location(ChaineConnexion).Modifier(ID_location, ID_client, dat_location); }
+
+        public List<C_TB_location> Lire(string Index)
+        { return new A_TB_location(ChaineConnexion).Lire(Index); }
+
+        public C_TB_location Lire_ID(int ID_location)
+        { return new A_TB_location(ChaineConnexion).Lire_ID(ID_location); }
+
+        public int Supprimer(int ID_location)
+        { return new A_TB_location(ChaineConnexion).Supprimer(ID_location); }
+    }
 }
